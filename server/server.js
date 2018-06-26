31d81b6c206a96356ceb714b3496c0dc6cfb011c
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-06-26 19:25:43 +0200

    creamos carpeta routes y dentro de routes fichero usuario.js e index.js. En index.js pondremos las rutas de la API

--- server/server.js
+++ server/server.js
@@ -8,47 +8,13 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
 // Conect to BBDD MongoDB
 mongoose.connect('mongodb://localhost:27017/Kubide', (err, res) => {
