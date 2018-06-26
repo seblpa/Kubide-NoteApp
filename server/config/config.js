c5a5027b56b345923297486c74aaed949ba18153
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-06-26 20:00:02 +0200

    rutas usuarios + fichero config.js + underscore + verificaToken + verifica ADMIN_ROLE

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,30 @@
 // Puerto localhost:3000
 // ======================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================================
+// Entorno para desarrollo en EROKU
+// ======================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ======================================
+// Vencimiento del token: 30 días
+// ======================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ======================================
+// SEED
+// ======================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+// ======================================
+// BASE DE DATOS - para mlab
+// ======================================
+let urlDB;
