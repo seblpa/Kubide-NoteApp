dc74727df3096c24bd82c5b0eb082869b1546d6a
Sébastien Del Mestre <sebastien.delmestre@gmail.com>
2018-06-26 19:33:49 +0200

    creamos modelo de usuario, instalamos bcrypt y uniqueValidator y configuramos los roles permitidos

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const bcrypt = require('bcrypt');
+
+const Usuario = require('../models/usuario');
+
 const app = express();
 
 // ===================================================
